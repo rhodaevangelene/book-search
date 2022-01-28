19d4954a6c75971ba7946702ec7378dfe3f034ec
Rhoda Evangelene <rhoda.evangelene@gmail.com>
2022-01-27 23:14:23 -0500

    Added apollo server

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = "mysecretssshhhh";
 const expiration = '2h';
 
 module.exports = {
@@ -16,21 +16,19 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
